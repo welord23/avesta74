403c73998852501c1f785d08a22a2ce46523d835
rsx7 <rsx7@ymail.com>
2012-04-10 17:21:12 +0000

    "hicks!" should be yellow

--- src/creature.cpp
+++ src/creature.cpp
@@ -274,7 +274,7 @@ void Creature::onWalk(Direction& dir)
 					break;
 			}
 
-			g_game.internalCreatureSay(this, SPEAK_MONSTER_SAY, "Hicks!");
+			g_game.internalCreatureSay(this, SPEAK_SAY, "Hicks!");
 		}
 	}
 }
